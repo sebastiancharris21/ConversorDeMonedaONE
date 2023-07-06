74b5a0d5e516ae9a315f7cb002d7afd133368fe2
Sebastiancharris21 <Sebastiancharris21@gmail.com>
2023-07-06 13:07:10 -0500

    Proyecto ChallengeJAVA Conversor De Monedas

---
+++ src/com/mycompany/conversormoneda/logica/Control.java
@@ -0,0 +1,12 @@
+package com.mycompany.conversormoneda.logica;
+
+import com.mycompany.conversormoneda.gui.PrincipalScreen;
+
+public class Control {
+    public static void main(String[] args) {
+        PrincipalScreen screen = new PrincipalScreen();
+        
+        screen.setVisible(true);
+        screen.setLocationRelativeTo(null);
+    }
+}
